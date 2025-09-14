de252a911c89e98261689337bde3b771b73c9b87
Aman Kumar <ak222760@gmail.com>
2025-09-14 11:13:55 +0530

    Added some sample for cloud deploy

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'logoswayatt.png'));
 });
 
+app.get("/msg",(req,res)=>{
+  res.send("Hello from the Participants")
+});
+
+app.use("/reply",(req,res)=>{
+  res.send("Checking for the purpose of the deployment on aws and cloud not for futher use.")
+})
+
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
